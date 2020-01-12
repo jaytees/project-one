ca899ec6aba061c0219194bc930a144dd944a5f6
Jordan Trickett <jordan1trickett@gmail.com>
2020-01-12 18:50:51 +1100

    product index by most requested created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,25 @@ class ProductsController < ApplicationController
   end
 
   def index
+    # most requested items
+    # add sort by location facility
+
+    @products = Product.all.sort_by { | product_count |  product_count.line_items.count }.reverse  #sorts by most appearances in line items
+
+    # product.line_items.count 1 ?  'drop' : 'drops'
+
+    # .each do | product |
+    #  p product.name
+    #    if product.line_items.present?
+    #      # p product.line_items
+    #      total_requested = 0
+    #       product.line_items.each do | line |
+    #         total_requested += line.quantity
+    #       end
+    #       p "Total requested #{total_requested}"
+    #    end
+    # end;
+
   end
 
   def show
