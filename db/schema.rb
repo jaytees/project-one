1238e4139c94f6e5a9f54237d160fdc2546eeb4a
Jordan Trickett <jordan1trickett@gmail.com>
2020-01-16 16:43:43 +1100

    removed map test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_15_041923) do
+ActiveRecord::Schema.define(version: 2020_01_11_101705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(version: 2020_01_15_041923) do
     t.text "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "address"
-    t.float "latitude"
-    t.float "longitude"
   end
 
 end
